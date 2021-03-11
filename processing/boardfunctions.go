f41e59d79654c895d6f94f3da397633339f85ed5
Alistair Burrowes <alistair.burrowes@gmail.com>
2021-03-11 12:44:05 +1100

    Turn functions done

--- processing/boardfunctions.go
+++ processing/boardfunctions.go
@@ -4,6 +4,36 @@ import (
 	"golang-toy-robot/model"
 )
 
-func turnRight(dir model.Direction) model.Direction {
-	return model.North
+func turnRight(direction model.Direction) model.Direction {
+	var result model.Direction
+
+	switch direction {
+	case model.North:
+		result = model.East
+	case model.East:
+		result = model.South
+	case model.South:
+		result = model.West
+	case model.West:
+		result = model.North
+	}
+
+	return result
+}
+
+func turnLeft(direction model.Direction) model.Direction {
+	var result model.Direction
+
+	switch direction {
+	case model.North:
+		result = model.West
+	case model.West:
+		result = model.South
+	case model.South:
+		result = model.East
+	case model.East:
+		result = model.North
+	}
+
+	return result
 }
