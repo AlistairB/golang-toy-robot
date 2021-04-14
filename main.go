cb743e7f6efb7e462c0b3a571bc5c7e8f68bfb52
Alistair Burrowes <alistair.burrowes@gmail.com>
2021-04-14 14:12:42 +1000

    Code written. Not working. Will write tests

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	// reader := bufio.NewReader(os.Stdin)
 	table := model.Table{ MaxCoordinate: model.Coordinate{5,5}, Robot: nil}
 
 	fmt.Println("~~ Toy Robot ~~")
@@ -18,8 +17,8 @@ func main() {
 
 		toRunCommand := processing.ParseCommand(readCommandAsText)
 
-		table = processing.ProcessCommand(table, toRunCommand)
+		if toRunCommand != nil {
+			table = processing.ProcessCommand(table, toRunCommand)
+		}
 	}
-	// filename := os.Getenv("ROBOT_FILE")
-	// ui.Play(filename)
 }
--- processing/commands.go
+++ processing/commands.go
@@ -2,6 +2,8 @@ package processing
 
 import (
 	"golang-toy-robot/model"
+	"regexp"
+	"strconv"
 )
 
 func ParseCommand(input string) model.Command {
@@ -14,8 +16,59 @@ func ParseCommand(input string) model.Command {
 		result = model.LeftCommand{}
 	case "RIGHT":
 		result = model.RightCommand{}
-	case "Report":
+	case "REPORT":
 		result = model.ReportCommand{}
+	default:
+		result = parsePlace(input)
+	}
+
+	return result
+}
+
+var matchPlace = regexp.MustCompile(`^PLACE\s(?P<X>\d+),(?P<Y>\d+),(?P<Facing>[A-Z]+)$`)
+
+func parsePlace(input string) model.Command {
+	parseResult := matchPlace.FindStringSubmatch(`PLACE 1,1,NORTH`)
+
+	if len(parseResult) == 4 {
+		parsedX := parseCoordinateAxis(parseResult[1])
+		parsedY := parseCoordinateAxis(parseResult[2])
+		parsedFacing := parseFacing(parseResult[3])
+
+		if parsedX != nil && parsedY != nil && parsedFacing != nil {
+			return model.PlaceCommand{ Coordinate: model.Coordinate{ X: *parsedX, Y: *parsedY }, Facing: *parsedFacing }
+		}
+	}
+
+	return nil
+}
+
+func parseCoordinateAxis(input string) *int {
+	i, err := strconv.Atoi(input)
+
+	if err == nil {
+		return &i
+	} else {
+		return nil
+	}
+}
+
+func parseFacing(input string) *model.Direction {
+	var result *model.Direction
+
+	switch input {
+	case "NORTH":
+		d := model.North
+		result = &d
+	case "EAST":
+		d := model.East
+		result = &d
+	case "South":
+		d := model.South
+		result = &d
+	case "West":
+		d := model.West
+		result = &d
 	}
 
 	return result
